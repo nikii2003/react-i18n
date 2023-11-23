0258ace9787f80baba6db2e018b1a41b3db7dfca
Nikita kadiwike <ninavenikita492@gmail.com>
2023-11-23 19:40:19 +0530

    added css style

--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -8,23 +8,29 @@ function Home (){
     const [lang,setLang]=useState("en")
     return(
  <div>
+    <h1 className="heading-i18">Internationalization - I18n</h1><br/><br></br>
+    <div className="container-of-text">
     <h1>{I18n("welcomeMessage")}</h1>
-    <h1>{I18n("normalMessage")}</h1>
-    <h3>{I18n("greetingMessage")} </h3>
-
-<select 
+    <p>{I18n("normalMessage")}</p>
+    <h2>{I18n("greetingMessage")} </h2>
+   
+<select className="select-box"
 defaultValue={localStorage.getItem("lang")} 
 onChange={(e)=>{localStorage.setItem("lang",e.target.value)
 window.location.reload()}}>
     
     <option value="en">English</option>
-    <option value="mr">Marathi</option>
     <option value="hi">Hindi</option>
-</select>
-    <p>
+    <option value="mr">Marathi</option>
+    
+</select >
+
+</div>
+    <p className="userstatmessage">
 {I18n("userStatMessage", "<studentcount>",usercounter)}
     </p>
- </div>
+    </div>
+ 
     )
 }
 export default Home 
